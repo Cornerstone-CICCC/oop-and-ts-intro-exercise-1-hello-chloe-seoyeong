f51dec7bc19d9ea6d8ed6f73e0d771ec7caab8de
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-01-14 12:31:18 -0800

    Completed exercise 2

--- exercise2.js
+++ exercise2.js
@@ -7,14 +7,33 @@
 
 class BankAccount {
   // YOUR CODE HERE
+  constructor(account, balance) {
+    this.accountNumber = account;
+    this.balance = balance;
+  }
+  deposit(amount) {
+    this.balance += amount;
+  }
+  withdraw(amount) {
+    if(this.balance < amount) {
+      return;
+    }
+    this.balance -= amount;
+  }
+  static compareAccounts(account1, account2) {
+    if(account1.balance < account2.balance) {
+      return account2;
+    } else {
+      return account1;
+    }
+  }
 }
-
 
 // TEST CASE / DRIVER CODE
 const account1 = new BankAccount(123456, 500);
 const account2 = new BankAccount(654321, 1000);
 account1.deposit(300); // account1 becomes 800
-account2.withdraw(300); // account2 becomes 700
+account2.withdraw(300); // account2 becomes 700\
 const richerAccount = BankAccount.compareAccounts(account1, account2);
 console.log(`Account ${richerAccount.accountNumber} has the higher balance.`);
 // Expected output: "Account 123456 has the higher balance."
